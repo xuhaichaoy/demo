4ab944b526bfa4e4a504e3ecf3b11470c2ba2006
海超 <haichao@66rpg.com>
2020-12-16 11:45:18 +0800

    home

--- src/page/web/home/home.js
+++ src/page/web/home/home.js
@@ -6,7 +6,7 @@ import { withRouter } from "react-router-dom";
 // import { DatePicker } from 'antd-mobile';
 import "./home.css";
 
-import { SearchBar, Button, WingBlank, Tabs, WhiteSpace, Badge, PullToRefresh } from 'antd-mobile';
+import { SearchBar, Button, WingBlank, Tabs, WhiteSpace, Badge, PullToRefresh, Icon } from 'antd-mobile';
 
 
 class Search extends React.Component {
@@ -27,74 +27,141 @@ class Search extends React.Component {
 const tabs = [
   { title: <Badge>关注</Badge> },
   { title: <Badge>推荐</Badge> },
-//   { title: <Badge dot>Third Tab</Badge> },
+  //   { title: <Badge dot>Third Tab</Badge> },
 ];
 
 function genData() {
-    const dataArr = [];
-    for (let i = 0; i < 20; i++) {
-      dataArr.push(i);
-    }
-    return dataArr;
+  const dataArr = [];
+  for (let i = 0; i < 20; i++) {
+    dataArr.push(i);
+  }
+  return dataArr;
 }
 
-class TabExample extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            refreshing: false,
-            down: true,
-            height: document.documentElement.clientHeight,
-            data: [],
-        };
-    }
-    componentDidMount() {
-        const hei = this.state.height - ReactDOM.findDOMNode(this.ptr).offsetTop;
-        setTimeout(() => this.setState({
-            height: hei,
-            data: genData(),
-        }), 0);
-    }
-    render() {
-        return (
-            <div className="tabsIndex">
-                <Tabs tabs={tabs}
-                initialPage={1}
-                onChange={(tab, index) => { console.log('onChange', index, tab); }}
-                onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
-                >
-                <div className = "tabItem" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                <PullToRefresh
-                    damping={60}
-                    ref={el => this.ptr = el}
-                    style={{
-                    height: this.state.height,
-                    overflow: 'auto',
-                    }}
-                    indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
-                    direction={this.state.down ? 'down' : 'up'}
-                    refreshing={this.state.refreshing}
-                    onRefresh={() => {
-                        this.setState({ refreshing: true });
-                        setTimeout(() => {
-                            this.setState({ refreshing: false });
-                        }, 1000);
-                    }}
-                >
-                    {this.state.data.map(i => (
-                    <div key={i} style={{ textAlign: 'center', padding: 20 }}>
-                        {this.state.down ? 'pull down' : 'pull up'} {i}
+class TabExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      down: true,
+      height: document.documentElement.clientHeight,
+      data: [],
+    };
+  }
+  componentDidMount() {
+    const hei = this.state.height - 87.5;
+
+    console.log(hei, ReactDOM.findDOMNode(this.ptr).offsetTop, ReactDOM.findDOMNode(this.ptr))
+    setTimeout(() => this.setState({
+      height: hei,
+      data: genData(),
+    }), 0);
+  }
+  render() {
+    return (
+      <div className="tabsIndex">
+        <Tabs tabs={tabs}
+          initialPage={1}
+          onChange={(tab, index) => { console.log('onChange', index, tab); }}
+          onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+        >
+          <div className="tabItem" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+            <PullToRefresh
+              damping={60}
+              ref={el => this.ptr = el}
+              style={{
+                height: this.state.height,
+                overflow: 'auto',
+              }}
+              indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
+              direction={this.state.down ? 'down' : 'up'}
+              refreshing={this.state.refreshing}
+              onRefresh={() => {
+                this.setState({ refreshing: true });
+                setTimeout(() => {
+                  this.setState({ refreshing: false });
+                }, 1000);
+              }}
+            >
+              {this.state.data.map(i => (
+                <div key={i} style={{ textAlign: 'center', padding: 20 }}>
+                  {this.state.down ? 'pull down' : 'pull up'} {i}
+                </div>
+              ))}
+            </PullToRefresh>
+          </div>
+          <div className="tabItem" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+            <PullToRefresh
+              damping={60}
+              ref={el => this.ptr = el}
+              style={{
+                height: this.state.height,
+                overflow: 'auto',
+              }}
+              indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
+              direction={this.state.down ? 'down' : 'up'}
+              refreshing={this.state.refreshing}
+              onRefresh={() => {
+                this.setState({ refreshing: true });
+                setTimeout(() => {
+                  this.setState({ refreshing: false });
+                }, 1000);
+              }}
+            >
+              {this.state.data.map(i => (
+                <div key={i} className="cardItem">
+                  <div className="cardTopBox">
+                    <div className="cardImg">
+                      <img src="//wcdn1.cgyouxi.com/avatar/55865771_1594108289_middle.jpg" />
+                      <div className="cardNameBox">
+                        <div className="cardName">
+                          橙子🍊
+                        </div>
+                        <div className="cardSchool">
+                          北京大学2018级
+                        </div>
+                      </div>
+
+                      <div className="collectBtn">
+                        <Icon type="ellipsis" />
+                      </div>
                     </div>
-                    ))}
-                </PullToRefresh>
+                  </div>
+                  <div className="cardContent">
+                    <div className="cardWords">
+                      魔教少主如何追杀正道？
+                      “大家好，我叫枕玉，因为偷偷修魔被师尊推下山崖结束了罪e的一生。但是我不会就这样轻易地狗带！现征集好心人给我打999999灵力让我一刀9999999，一拳一个小朋友，事成之后我给你个天蓬元帅当当。”（划掉）
+                      本片又名《如何看待魔教少主被一群正道包围不知所措》
+                      枕玉大难不死，为了拿到藏在秘密禁地的力量，化名青禾与自己带着“正道buff”的仇人一起走上了寻秘之路。在这次意料之外的旅途之中，枕玉找到了杀死“正道”的办法……
+                    </div>
+                    <div className = "cardContentImgBox">
+                      <img src = "//pic.cgyouxi.com/orange/upload/202012/79031554_2fa4ff1e09657db2a72f3d4f3b2ef859.jpg!n300" className="cardContentImgItem" />
+                      <img src = "//pic.cgyouxi.com/orange/upload/202012/7545149_d2db5203a9b689fac7f25cbaa23b3040.png!n300" className="cardContentImgItem" />
+                      <img src = "//pic.cgyouxi.com/orange/upload/202012/7545149_d2db5203a9b689fac7f25cbaa23b3040.png!n300" className="cardContentImgItem" />
+                    </div>
+                  </div>
+                  <div className="cardBtnsBox">
+                    <div className="cardBtnsItem">
+                      <i className="zan"></i>
+                      <span>6</span>
+                    </div>
+                    <div className="cardBtnsItem">
+                      <i className="comment"></i>
+                      <span>2</span>
+                    </div>
+                    <div className="cardBtnsItem">
+                      <i className="face"></i>
+                      <span>0</span>
+                    </div>
+                  </div>
                 </div>
-                <div className = "tabItem" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                    Content of second tab
-                </div>
-                </Tabs>
-            </div>
-        )
-    }
+              ))}
+            </PullToRefresh>
+          </div>
+        </Tabs>
+      </div>
+    )
+  }
 }
 
 
@@ -106,23 +173,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     }
   };
 
   componentDidMount() {
- 
+
   }
   componentWillReceiveProps(prevProps, prevState) {
-   
+
   }
 
   render() {
     return (
-        <div>
-            <Search />
-            <TabExample />
-        </div>
+      <div>
+        <Search />
+        <TabExample />
+      </div>
     );
   }
 }
