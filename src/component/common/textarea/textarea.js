4ca9e8bb4bfc9a563e2411fa8b797cfd2e4b51c0
海超 <haichao@66rpg.com>
2021-02-18 18:00:18 +0800

    detail

---
+++ src/component/common/textarea/textarea.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { List, TextareaItem } from 'antd-mobile';
+import { createForm } from 'rc-form';
+import "./textarea.css";
+class TextareaItemExample extends React.Component {
+    componentDidMount() {
+    }
+    render() {
+        const { getFieldProps } = this.props.form;
+        return (
+            <div className = "textareaBox">
+                <List renderHeader={() => '回复海超'}>
+                    <TextareaItem
+                        {...getFieldProps('count', {
+                            // initialValue: '',
+                        })}
+                        placeholder="说点什么呢..."
+                        rows={5}
+                        count={120}
+                        className = "textarea"
+                    />
+                </List>
+
+            </div>
+        );
+    }
+}
+
+const TextareaItemExampleWrapper = createForm()(TextareaItemExample);
+export default TextareaItemExampleWrapper
