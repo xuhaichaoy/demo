ede88a2c50cb2bdb6d821ac3d815fe0cb001a869
海超 <xuhaichaoy@163.com>
2021-02-19 20:49:49 +0800

    comment

--- src/component/common/comment/comment.js
+++ src/component/common/comment/comment.js
@@ -13,7 +13,7 @@ class Comment extends React.Component {
                     <Item>
                         <div className="MainComment">
                             <div className="MainUserImage">
-                                <img src = "//wcdn1.cgyouxi.com/avatar/4821709_1604752251_middle.jpg" className = "userImage"/>
+                                <img src="//wcdn1.cgyouxi.com/avatar/4821709_1604752251_middle.jpg" className="userImage" />
                             </div>
                             <div className="MainConnent">
                                 <div className="MainUserName">
@@ -32,39 +32,39 @@ class Comment extends React.Component {
                                     <div className="MainCommentCampus">
                                         来自: 北京大学
                                     </div>
-                                    <div className = "MainCommentZan">
+                                    <div className="MainCommentZan">
                                         <i></i>
                                         <span>0</span>
                                     </div>
-                                    <div className = "MainCommentReply">
+                                    <div className="MainCommentReply">
                                         <i></i>
                                     </div>
-                                    <div className = "MainCommentMore">
+                                    <div className="MainCommentMore">
                                         <i></i>
                                     </div>
                                 </div>
-                                <div className = "MainCommentItem">
-                                    <div className = "CommentItemnent">
-                                        <div className = "commentItemUser">
+                                <div className="MainCommentItem">
+                                    <div className="CommentItemnent">
+                                        <div className="commentItemUser">
                                             按实际宽度哈萨：
                                         </div>
-                                        <div className = "commentItemcomment">
-                                            sadfasdfasdfasdfaaaaaaaaaaaaasdfadsf
+                                        <div className="commentItemcomment">
+                                            哈哈哈哈哈哈哈
                                         </div>
                                     </div>
-                                    <div className = "CommentItemnent">
-                                        <div className = "commentItemUser">
+                                    <div className="CommentItemnent">
+                                        <div className="commentItemUser">
                                             按实际宽度哈萨：
                                         </div>
-                                        <div className = "commentItemcomment">
-                                            sadfasdfasdfasdfaaaaaaaaaaaaasdfadsf
+                                        <div className="commentItemcomment">
+                                            你好厉害
                                         </div>
-                                    </div><div className = "CommentItemnent">
-                                        <div className = "commentItemUser">
+                                    </div><div className="CommentItemnent">
+                                        <div className="commentItemUser">
                                             按实际宽度哈萨：
                                         </div>
-                                        <div className = "commentItemcomment">
-                                            sadfasdfasdfasdfaaaaaaaaaaaaasdfadsf
+                                        <div className="commentItemcomment">
+                                            啊啊啊啊啊啊
                                         </div>
                                     </div>
                                 </div>
@@ -74,35 +74,60 @@ class Comment extends React.Component {
                     <Item>
                         <div className="MainComment">
                             <div className="MainUserImage">
-
+                                <img src="//wcdn1.cgyouxi.com/avatar/4821709_1604752251_middle.jpg" className="userImage" />
                             </div>
                             <div className="MainConnent">
                                 <div className="MainUserName">
-
+                                    namesadkjhasdha
                                 </div>
                                 <div className="MainSendTime">
-
+                                    2021-09-12 12:12:12
                                 </div>
                                 <div className="MainCommentContent">
-
+                                    就爱看还是了了了了了了了了了了了了了了按时间客户啦啦啦啦啦啦啦啦啦啦啦啦啦啦看见撒旦了可拉
+                                    速度发货时间的官方京哈高速的发士大夫斯基的发货卡上建立的回复课件撒好多饭喀什地方
+                                    啊士大夫看见哈卡撒娇的回复凯撒后端开发哈萨克的回复卡号是的看哈萨克的发货
                                 </div>
 
                                 <div className="MainCommentBottom">
                                     <div className="MainCommentCampus">
-
+                                        来自: 北京大学
                                     </div>
-                                    <div className = "MainCommentZan">
-                                        
+                                    <div className="MainCommentZan">
+                                        <i></i>
+                                        <span>0</span>
                                     </div>
-                                    <div className = "MainCommentReply">
-                                        
+                                    <div className="MainCommentReply">
+                                        <i></i>
                                     </div>
-                                    <div className = "MainCommentMore">
-                                        
+                                    <div className="MainCommentMore">
+                                        <i></i>
                                     </div>
                                 </div>
-                                <div className = "MainCommentItem">
-                                    
+                                <div className="MainCommentItem">
+                                    <div className="CommentItemnent">
+                                        <div className="commentItemUser">
+                                            按实际宽度哈萨：
+                                        </div>
+                                        <div className="commentItemcomment">
+                                            哈哈哈哈哈哈哈
+                                        </div>
+                                    </div>
+                                    <div className="CommentItemnent">
+                                        <div className="commentItemUser">
+                                            按实际宽度哈萨：
+                                        </div>
+                                        <div className="commentItemcomment">
+                                            你好厉害
+                                        </div>
+                                    </div><div className="CommentItemnent">
+                                        <div className="commentItemUser">
+                                            按实际宽度哈萨：
+                                        </div>
+                                        <div className="commentItemcomment">
+                                            啊啊啊啊啊啊
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -110,35 +135,60 @@ class Comment extends React.Component {
                     <Item>
                         <div className="MainComment">
                             <div className="MainUserImage">
-
+                                <img src="//wcdn1.cgyouxi.com/avatar/4821709_1604752251_middle.jpg" className="userImage" />
                             </div>
                             <div className="MainConnent">
                                 <div className="MainUserName">
-
+                                    namesadkjhasdha
                                 </div>
                                 <div className="MainSendTime">
-
+                                    2021-09-12 12:12:12
                                 </div>
                                 <div className="MainCommentContent">
-
+                                    就爱看还是了了了了了了了了了了了了了了按时间客户啦啦啦啦啦啦啦啦啦啦啦啦啦啦看见撒旦了可拉
+                                    速度发货时间的官方京哈高速的发士大夫斯基的发货卡上建立的回复课件撒好多饭喀什地方
+                                    啊士大夫看见哈卡撒娇的回复凯撒后端开发哈萨克的回复卡号是的看哈萨克的发货
                                 </div>
 
                                 <div className="MainCommentBottom">
                                     <div className="MainCommentCampus">
-
+                                        来自: 北京大学
                                     </div>
-                                    <div className = "MainCommentZan">
-                                        
+                                    <div className="MainCommentZan">
+                                        <i></i>
+                                        <span>0</span>
                                     </div>
-                                    <div className = "MainCommentReply">
-                                        
+                                    <div className="MainCommentReply">
+                                        <i></i>
                                     </div>
-                                    <div className = "MainCommentMore">
-                                        
+                                    <div className="MainCommentMore">
+                                        <i></i>
                                     </div>
                                 </div>
-                                <div className = "MainCommentItem">
-                                    
+                                <div className="MainCommentItem">
+                                    <div className="CommentItemnent">
+                                        <div className="commentItemUser">
+                                            按实际宽度哈萨：
+                                        </div>
+                                        <div className="commentItemcomment">
+                                            哈哈哈哈哈哈哈
+                                        </div>
+                                    </div>
+                                    <div className="CommentItemnent">
+                                        <div className="commentItemUser">
+                                            按实际宽度哈萨：
+                                        </div>
+                                        <div className="commentItemcomment">
+                                            你好厉害
+                                        </div>
+                                    </div><div className="CommentItemnent">
+                                        <div className="commentItemUser">
+                                            按实际宽度哈萨：
+                                        </div>
+                                        <div className="commentItemcomment">
+                                            啊啊啊啊啊啊
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
